82cd4e8a61b2af398972d5795c14348714104939
asinha94 <asinha94@gmail.com>
2019-11-04 03:29:33 +0000

    added interfaces

--- include/client.h
+++ include/client.h
@@ -3,15 +3,16 @@
 
 #include <cstdint>
 
-namespace xsty {
+namespace xausty {
     // Client inbound connections
     class ClientConnection {
     public:
         ClientConnection(int32_t fd);
         bool read(const char * data);
         bool write(const char * data);
+        int32_t get_fd();
     private:
-        int32_t fd;
+        const int32_t fd;
         bool can_read;
         bool can_write;
         int32_t state;  
--- include/epoll.h
+++ include/epoll.h
@@ -1,10 +1,24 @@
 #ifndef XSTY_EPOLL_H
 #define XSTY_EPOLL_H
 
-namespace xsty {
+#include <cstdint>
+#include <sys/epoll.h>
+
+namespace xausty {
 
     class Epoll {
-
+        using EpollEvent = struct epoll_event;
+        using EpollEventPtr = struct epoll_event *;
+    public:
+        Epoll(int32_t fd, int32_t max_events);
+        bool start_watching(int32_t fd, bool readble, bool writeable));
+        bool stop_watching(int32_t fd);;
+        int32_t wait();
+    private:
+        int32_t fd;
+        const int32_t max_events;
+        EpollEvent ev;
+        EpollEventPtr events;
     };
 
 }
